0616c0112d01f731a1ab028fb0e85a452a56279e
anggaaja <anggaaja@users.noreply.github.com>
2016-06-07 16:38:24 +0700

    XMedia Recode Initial Commit
    Initial Commit for XMedia Recode

---
+++ xmedia-recode/tools/chocolateyinstall.ps1
@@ -0,0 +1,38 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$packageName = 'xmedia-recode'
+$version = '3325'
+$url        = 'http://www.xmedia-recode.de/download/XMediaRecode${version}_setup.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  $silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  $validExitCodes = @(0)
+  checksum      = '7583A091158115055832E89DA0CC47BF'
+  checksumType  = 'md5'
+  
+
+
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
