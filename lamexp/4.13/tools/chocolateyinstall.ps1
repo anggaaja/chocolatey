817f4b2f47d59b6fe8d33043aa6a42e88a9aab7f
anggaaja <anggaaja@users.noreply.github.com>
2016-06-07 19:49:34 +0700

    LameXP v4.13 Initial Commit
    Initial Commit for LameXP v4.13

---
+++ lamexp/4.13/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+$ErrorActionPreference = 'Stop';
+$packageName = 'lamexp'
+$version = '4.13'
+$url = 'https://github.com/lordmulder/LameXP/releases/download/Release_413/LameXP.2015-12-12.Release-Static.Build-1852.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs = '/S'
+  validExitCodes = @(0)
+  checksum      = '7929A60D4E785494D95DB195CCED4AE1'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
