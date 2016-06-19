97c6e986c9ad4856cb7ed82e72936b98bc16c852
anggaaja <anggaaja@users.noreply.github.com>
2016-06-19 20:25:26 +0700

    XMedia Recode 3.3.2.8
    Updated XMedia Recode to 3.3.2.8

---
+++ xmedia-recode/3.3.2.8/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'xmedia-recode'
+$version = '3.3.2.8'
+$url        = 'http://www.xmedia-recode.de/download/XMediaRecode3328_setup.exe'
+
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+  checksum      = 'A9585939FAC052FDF4987FFBDC16B4D1'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
